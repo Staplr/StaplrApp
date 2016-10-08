120f185b6fa01e5b3bc6ae56da63f014c9a1c948
kwuz <young.mikaila@gmail.com>
2016-10-08 03:44:33 -0600

    ???

--- Staplr/Resources/Screens/CourseList.cs
+++ Staplr/Resources/Screens/CourseList.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Staplr.Common;
 
 using Android.App;
 using Android.Content;
@@ -10,9 +11,19 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
-namespace Staplr.Resources.layout
+namespace Staplr.Resources.Screens
 {
-    class CourseList
+    [Activity(Label = "CourseTable", MainLauncher = true)]
+    public class CourseList : ListActivity
     {
+        string[] items;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            items = new string[] { "test", "another test", "wheeeetests" };
+            ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
+
+        }
+        
     }
 }
---
+++ Staplr/Resources/Screens/CourseListAdapter.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Staplr.Resources.layout
+{
+    class CourseListAdapter : BaseAdapter<string>
+    {
+        public override string this[int position]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
